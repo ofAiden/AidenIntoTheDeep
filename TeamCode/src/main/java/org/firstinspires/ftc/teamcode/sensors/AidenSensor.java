1414a9454668ece5bde3a7658b000893378091cb
aiden <aiden.lee.sd@gmail.com>
2025-07-01 16:23:11 -0700

    funny sensors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sensors/AidenSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sensors/AidenSensor.java
@@ -1,4 +1,29 @@
 package org.firstinspires.ftc.teamcode.sensors;
+import org.firstinspires.ftc.teamcode.utils.priority.PriorityMotor;
+import org.firstinspires.ftc.teamcode.AidenRobot;
 
 public class AidenSensor {
+    private final AidenRobot robot;
+    private int turretEncoder;
+    private int turretZero;
+    public double turretAnglePerTick;
+
+    private double voltage;
+    //the time we want to have before updating each time
+    private final double voltageUpdateTime = 5000;
+    private long lastVoltageUpdatedTime = System.currentTimeMillis();
+
+    public AidenSensor(AidenRobot robot) {
+        this.robot = robot;
+    }
+    public void update(){
+        turretEncoder = ((PriorityMotor) robot.hardwareQueue.getDevice("turret")).motor[0].getCurrentPosition();
+        if((System.currentTimeMillis()-lastVoltageUpdatedTime)>voltageUpdateTime){
+            voltage = robot.hardwareMap.voltageSensor.iterator().next().getVoltage();
+            lastVoltageUpdatedTime = System.currentTimeMillis() ;
+        }
+    }
+    public double getTurretAngle(){
+        return (turretEncoder-turretZero) * turretAnglePerTick;
+    }
 }
