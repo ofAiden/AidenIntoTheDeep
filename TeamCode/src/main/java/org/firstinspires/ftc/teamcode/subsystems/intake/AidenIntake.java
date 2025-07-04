af88dcc26488bbc6eca168183d6a9bfb280611a1
therealkingcob <rishiashah19@gmail.com>
2025-07-04 15:51:52 -0700

    finished FSM

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/intake/AidenIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/intake/AidenIntake.java
@@ -14,21 +14,23 @@ public class AidenIntake {
 
     private final PriorityMotor extendo;
     private final PriorityMotor intake_motor;
-    private final nPriorityServo extendo1, extendo2, vbar;
+    private final nPriorityServo pivot;
     private final AidenRobot robot;
 
     public static PID extendo_pid = new PID(1, 0, 0); //tune ts
 
     private double extendo_target_pos;
     private double extendo_current_pos = 0;
+    private double extendo_error;
+
+    private double extendo_pow = 0;
+    private double extendo_keep_in_pow = -0.2;
 
 
 
 
     public enum IntakeStates {
-        START,
         EXTEND,
-        DROP_DOWN,
         INTAKE,
         CONFIRM,
         RETRACT,
@@ -37,26 +39,52 @@ public class AidenIntake {
         REVERSE
     }
 
-    public IntakeStates intakeStates = IntakeStates.START;
+    public IntakeStates intakeStates = IntakeStates.IDLE;
 
     public AidenIntake(AidenRobot robot) {
         this.robot = robot;
         extendo = new PriorityMotor(robot.hardwareMap.get(DcMotorEx.class, "extendo"), "extendo", 2, 5, null);
         intake_motor = new PriorityMotor(robot.hardwareMap.get(DcMotorEx.class, "intake_motor"), "intake_motor", 2.5, 5, null);
-        extendo1 = new nPriorityServo(new Servo[]{robot.hardwareMap.get(Servo.class, "extendo1")}, "extendo1", nPriorityServo.ServoType.AXON_MAX, 0, 1, 0.5, new boolean[]{false}, 3, 5);
-        extendo2 = new nPriorityServo(new Servo[]{robot.hardwareMap.get(Servo.class, "extendo2")}, "extendo2", nPriorityServo.ServoType.AXON_MAX, 0, 1, 0.5, new boolean[]{false}, 3, 5);
-        vbar = new nPriorityServo(new Servo[]{robot.hardwareMap.get(Servo.class, "vbar1"), robot.hardwareMap.get(Servo.class, "vbar2")}, "vbar1", nPriorityServo.ServoType.AXON_MAX, 0, 1, 0.5, new boolean[]{false, true}, 2, 5);
-        robot.hardwareQueue.addDevices(intake_motor, extendo, extendo1, extendo2, vbar);
+        pivot = new nPriorityServo(new Servo[]{robot.hardwareMap.get(Servo.class, "pivot")}, "pivot", nPriorityServo.ServoType.AXON_MAX, 0, 1, 0.5, new boolean[]{false}, 3, 5);
+        robot.hardwareQueue.addDevices(intake_motor, extendo, pivot);
     }
 
 
     public void update() {
-        update_extendo();
+        this.update_extendo();
         switch(intakeStates) {
-            case START:
-                break;
-
+            case IDLE:
+                //idk how to handle this
             case EXTEND:
+                extendo_target_pos = 20; //random number idk yet
+                update_extendo();
+                extendo_error = extendo_target_pos - extendo_current_pos;
+                set_intake_pivot( Math.PI/5,1); // tune this value
+
+
+                if(extendo_error < 0.5) {
+                    intakeStates = IntakeStates.INTAKE;
+                }
+
+            case INTAKE:
+                set_intake_motor_power(1); //tune this
+                intakeStates = IntakeStates.CONFIRM;
+            case CONFIRM:
+                //if the color sensor detects the right block and its in, we can switch states to retract
+                //otherwise go back to intake state
+            case RETRACT:
+                extendo_target_pos = 0;
+                update_extendo();
+                set_intake_pivot(Math.PI, 1); //tune this value
+
+                if(get_extendo_pos() < 0.1 && pivot.inPosition()) {
+                    intakeStates = IntakeStates.TRANSFER;
+                }
+
+            case TRANSFER:
+                //do transfer stuff
+            case REVERSE:
+                set_intake_motor_power(-1); // tune this
 
         }
 
@@ -64,10 +92,26 @@ public class AidenIntake {
     }
 
     public void update_extendo() {
-        extendo_current_pos = robot.sensor.get_extendo_pos();
-        double pow = 0;
-        pow = extendo_pid.update(extendo_target_pos - extendo_current_pos, -0.7, 0.7);
-        extendo.setTargetPower(pow);
+        if(extendo_current_pos > 0.5) {
+            extendo_current_pos = robot.sensor.get_extendo_pos();
+            extendo_error = extendo_target_pos - extendo_current_pos;
+            extendo_pow = extendo_pid.update(extendo_error, -0.7, 0.7);
+            extendo.setTargetPower(extendo_pow);
+        } else {
+            extendo.setTargetPower(extendo_keep_in_pow);
+        }
+    }
+
+    public double get_extendo_pos() {
+        return extendo_current_pos;
+    }
+
+    public void set_intake_pivot(double target_pos, double power) {
+        pivot.setTargetAngle(target_pos, power);
+    }
+
+    public void set_intake_motor_power(double power) {
+        intake_motor.setTargetPower(power);
     }
 
 
